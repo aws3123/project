294e869a4b58d3ef160b9168db83fd3c16954d17
aawawjh <16266190+aawawjh@user.noreply.gitee.com>
2026-09-18 00:03:35 +0800

    chore: untrack runtime data, drop debug probes, move ops scripts to scripts/

--- thread_names.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-PID=${1:-290704}
-echo "=== thread name distribution for PID $PID ==="
-for t in /proc/$PID/task/*; do
-  cat "$t/comm"
-done | sort | uniq -c | sort -rn | head -25
-echo ""
-echo "=== total threads ==="
-ls /proc/$PID/task | wc -l
